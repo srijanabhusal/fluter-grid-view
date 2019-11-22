04dc5d6ca3381f7f9eab7ab7a5c1d03d034973b4
srijanabhusal <072bex440@ioe.edu.np>
2019-11-22 11:20:31 +0545

    final codes

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+ import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Grid View';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: GridView.count(
+          // Create a grid with 2 columns. If you change the scrollDirection to
+          // horizontal, this produces 2 rows.
+          crossAxisCount: 2,
+          // Generate 100 widgets that display their index in the List.
+          children: List.generate(100, (index) {
+            return Center(
+              child: Text(
+                'Grid item no. $index',                
+                style: Theme.of(context).textTheme.headline,
+                
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
